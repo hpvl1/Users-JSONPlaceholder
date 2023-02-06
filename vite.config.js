a91919b4c1b309304f245fb7157fc2c3900368c2
Pavel <pvlwrk@gmail.com>
2023-02-06 16:46:52 +0300

    update base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/<REPO>/',
   plugins: [
     vue(),
     vuetify(),
